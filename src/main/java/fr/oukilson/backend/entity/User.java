484cc459290f308724885684f34f9abe7ac3177c
LMika <lemika.pro@gmail.com>
2022-02-10 15:59:21 +0100

    feat[User] : Add relation with the tables holding the list of owned game and the list of liked game. Add convenient methods to add/remove a game in these lists.

--- src/main/java/fr/oukilson/backend/entity/User.java
+++ src/main/java/fr/oukilson/backend/entity/User.java
@@ -3,6 +3,7 @@ package fr.oukilson.backend.entity;
 import lombok.*;
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Entity
@@ -23,7 +24,7 @@ public class User {
     @Column(name = "last_name")
     private String lastName;            // Last name of the user
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinTable(name = "friend_list",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "friend_id"))
@@ -46,6 +47,81 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "game_id"))
     private List<Game> likedGame = new ArrayList<>();
+
+    /**
+     * Add a game into the owned game list
+     * @param game Game
+     * @return True if removed
+     */
+    public boolean addOwnedGame(Game game) {
+        return this.addGame(this.ownedGame, game);
+    }
+
+    /**
+     * Add a game into the liked game list
+     * @param game Game
+     * @return True if removed
+     */
+    public boolean addLikedGame(Game game) {
+        return this.addGame(this.likedGame, game);
+    }
+
+    /**
+     * Add a game into a game list
+     * @param list A Game list
+     * @param game Game
+     * @return True if removed
+     */
+    private boolean addGame(List<Game> list, Game game) {
+        boolean result = true;
+        Iterator<Game> it = list.iterator();
+        while (it.hasNext()) {
+            Game g = it.next();
+            if (game.getId()==g.getId()) {
+                result = false;
+                break;
+            }
+        }
+        if (result) list.add(game);
+        return result;
+    }
+
+    /**
+     * Remove a game from the owned game list
+     * @param game Game
+     * @return True if removed
+     */
+    public boolean removeOwnedGame(Game game) {
+        return this.removeGame(this.ownedGame, game);
+    }
+
+    /**
+     * Remove a game from the liked game list
+     * @param game Game
+     * @return True if removed
+     */
+    public boolean removeLikedGame(Game game) {
+        return this.removeGame(this.likedGame, game);
+    }
+
+    /**
+     * Remove a game from a game list
+     * @param list A Game list
+     * @param game Game
+     * @return True if removed
+     */
+    private boolean removeGame(List<Game> list, Game game) {
+        boolean result = false;
+        for(Iterator<Game> iter = list.iterator(); iter.hasNext();) {
+            Game data = iter.next();
+            if (data.getId()==game.getId()) {
+                iter.remove();
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 
     @Override
     public boolean equals(Object o) {
