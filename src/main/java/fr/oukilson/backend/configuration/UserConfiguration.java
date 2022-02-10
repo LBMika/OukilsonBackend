a689b04bd74e2bb0e28450cf9c33e444b61ab32e
LMika <lemika.pro@gmail.com>
2022-02-10 12:26:21 +0100

    fix[Event] : Add EventRepository in bean UserService

--- src/main/java/fr/oukilson/backend/configuration/UserConfiguration.java
+++ src/main/java/fr/oukilson/backend/configuration/UserConfiguration.java
@@ -1,6 +1,7 @@
 package fr.oukilson.backend.configuration;
 
 import fr.oukilson.backend.model.RegexCollection;
+import fr.oukilson.backend.repository.EventRepository;
 import fr.oukilson.backend.repository.UserRepository;
 import fr.oukilson.backend.service.UserService;
 import org.modelmapper.ModelMapper;
@@ -10,8 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class UserConfiguration {
     @Bean
-    public UserService userService(UserRepository userRepository, ModelMapper modelMapper,
-                                   RegexCollection regexCollection){
-        return new UserService(userRepository, modelMapper, regexCollection);
+    public UserService userService(UserRepository userRepository, EventRepository eventRepository,
+                                   ModelMapper modelMapper, RegexCollection regexCollection){
+        return new UserService(userRepository, eventRepository, modelMapper, regexCollection);
     }
 }
