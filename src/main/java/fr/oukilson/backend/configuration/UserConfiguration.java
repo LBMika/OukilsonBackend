a48a850288e3344f28937959753583d7ea5da36f
LMika <lemika.pro@gmail.com>
2022-02-10 16:01:32 +0100

    feat[User] : Add argument in UserService bean

--- src/main/java/fr/oukilson/backend/configuration/UserConfiguration.java
+++ src/main/java/fr/oukilson/backend/configuration/UserConfiguration.java
@@ -2,6 +2,7 @@ package fr.oukilson.backend.configuration;
 
 import fr.oukilson.backend.model.RegexCollection;
 import fr.oukilson.backend.repository.EventRepository;
+import fr.oukilson.backend.repository.GameRepository;
 import fr.oukilson.backend.repository.UserRepository;
 import fr.oukilson.backend.service.UserService;
 import org.modelmapper.ModelMapper;
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Configuration;
 public class UserConfiguration {
     @Bean
     public UserService userService(UserRepository userRepository, EventRepository eventRepository,
-                                   ModelMapper modelMapper, RegexCollection regexCollection){
-        return new UserService(userRepository, eventRepository, modelMapper, regexCollection);
+                                   GameRepository gameRepository, ModelMapper modelMapper,
+                                   RegexCollection regexCollection){
+        return new UserService(userRepository, eventRepository, gameRepository, modelMapper, regexCollection);
     }
 }
