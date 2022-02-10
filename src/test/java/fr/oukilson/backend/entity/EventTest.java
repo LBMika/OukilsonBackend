ad1278ae5354ee17f1969596a53c6a06faace64d
LMika <lemika.pro@gmail.com>
2022-02-10 10:34:42 +0100

    fix[Test] : Remove spaces

--- src/test/java/fr/oukilson/backend/entity/EventTest.java
+++ src/test/java/fr/oukilson/backend/entity/EventTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
