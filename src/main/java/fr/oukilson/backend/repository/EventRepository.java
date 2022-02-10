8c496fbd4ac95c07d71650ee3446c5e0bc466150
LMika <lemika.pro@gmail.com>
2022-02-10 12:25:44 +0100

    fix[Event] : Add Distinct

--- src/main/java/fr/oukilson/backend/repository/EventRepository.java
+++ src/main/java/fr/oukilson/backend/repository/EventRepository.java
@@ -15,6 +15,6 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAllByLocationTownContaining(String town);
     List<Event> findAllByCreatorId(long id);
     List<Event> findAllByRegisteredUsersId(long id);
-    List<Event> findAllByCreatorIdOrRegisteredUsersIdOrWaitingUsersId(long id1, long id2, long id3);
+    List<Event> findAllDistinctByCreatorIdOrRegisteredUsersIdOrWaitingUsersId(long id1, long id2, long id3);
     void deleteByUuid(String uuid);
 }
