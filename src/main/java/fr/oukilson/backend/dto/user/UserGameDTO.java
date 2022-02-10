d25112217411f205712f647008a1c3a11a7b6c72
LMika <lemika.pro@gmail.com>
2022-02-10 16:00:06 +0100

    feat[DTO] : Add DTO used to hold the game's uuid to add/remove from a game list

---
+++ src/main/java/fr/oukilson/backend/dto/user/UserGameDTO.java
@@ -0,0 +1,12 @@
+package fr.oukilson.backend.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserGameDTO {
+    private String uuid;
+}
