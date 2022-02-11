89c779374281e848627ba3040be18f17695bcea4
LMika <lemika.pro@gmail.com>
2022-02-11 09:49:40 +0100

    fix[Auth] : Add CORS configuration (fourth try)

--- src/main/java/fr/oukilson/backend/configuration/WebConfig.java
+++ src/main/java/fr/oukilson/backend/configuration/WebConfig.java
@@ -1,15 +1,21 @@
 package fr.oukilson.backend.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
