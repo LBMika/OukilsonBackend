664452530d68f350c7a9e0708592d92e38a73494
LMika <lemika.pro@gmail.com>
2022-02-15 12:58:02 +0100

    fix : Remove useless empty lines

--- src/main/java/fr/oukilson/backend/repository/EventRepository.java
+++ src/main/java/fr/oukilson/backend/repository/EventRepository.java
@@ -2,9 +2,7 @@ package fr.oukilson.backend.repository;
 
 import fr.oukilson.backend.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/fr/oukilson/backend/repository/UserRepository.java
+++ src/main/java/fr/oukilson/backend/repository/UserRepository.java
@@ -3,7 +3,6 @@ package fr.oukilson.backend.repository;
 import fr.oukilson.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 import java.util.Optional;
 
